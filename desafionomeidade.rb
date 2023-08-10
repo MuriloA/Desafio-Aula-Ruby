97e18a26098fbabef0f8c841159739f88aa54646
MuriloA <59372058+MuriloA@users.noreply.github.com>
2023-08-10 11:40:44 -0300

    Add files via upload

---
+++ desafionomeidade.rb
@@ -0,0 +1,7 @@
+print "digite seu nome: "
+nome = gets.chomp
+print "Digite seu sobrenome: "
+sobrenome = gets.chomp
+print "Digite sua idade: "
+idade = gets.chomp
+puts "Oi, seu nome é #{nome} #{sobrenome}, sua idade é #{idade} anos !"
